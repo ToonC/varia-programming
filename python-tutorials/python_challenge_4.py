24473e5335ba1634aa0dc995bd9228a0eecf704d
Toon <t.cuyvers1@gmail.com>
2016-11-21 23:15:40 +0100

    repeat with anaconda installed

--- python-tutorials/python_challenge_4.py
+++ python-tutorials/python_challenge_4.py
@@ -1,8 +1,11 @@
 from urllib import request
 from lxml import html
 
+uri = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={:d}"
+nothing = 12345
 
-with request.urlopen('http://python.org/') as response:
+
+with request.urlopen(uri.format(nothing)) as response:
    page = html.fromstring(response.read())
-
+   print(response.read())
    print(page)
