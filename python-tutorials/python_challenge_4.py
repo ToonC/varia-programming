39a88a5db17d9599e2aa8bacf31611a0aee22fdf
Toon <t.cuyvers1@gmail.com>
2016-11-21 22:13:54 +0100

    start using lxml to interpret the page data

--- python-tutorials/python_challenge_4.py
+++ python-tutorials/python_challenge_4.py
@@ -1,3 +1,8 @@
-import urllib.request
-with urllib.request.urlopen('http://python.org/') as response:
-   html = response.read()
+from urllib import request
+from lxml import html
+
+
+with request.urlopen('http://python.org/') as response:
+   page = html.fromstring(response.read())
+
+   print(page)
